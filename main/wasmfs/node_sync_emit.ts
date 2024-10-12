cb9fa278ea5ff701d50d9068e081fe6cb5a75854
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-12 17:22:18 -0500

    get wasmfs locally

---
+++ main/wasmfs/node_sync_emit.ts
@@ -0,0 +1,19 @@
+// https://cdn.jsdelivr.net/npm/@wasmer/wasmfs@0.12.0/src/node_sync_emit.ts
+import { Node } from "https://esm.sh/memfs@3.0.4/lib/node";
+
+// Make emit synchronous
+Node.prototype.emit = function(
+  this: Node,
+  event: string | symbol,
+  ...args: any[]
+): boolean {
+  const listeners = this.listeners(event);
+  for (let listener of listeners) {
+    try {
+      listener(...args);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  return listeners.length > 0;
+};
