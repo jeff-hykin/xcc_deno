c32d8be28b847ab64ffc933ed9733f7b8905375d
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-12 17:49:05 -0500

    fix compat problem

--- main/buffer_es6@4_9_3.js
+++ main/buffer_es6@4_9_3.js
@@ -37,8 +37,8 @@ export var INSPECT_MAX_BYTES = 50
  * We detect these buggy browsers and set `Buffer.TYPED_ARRAY_SUPPORT` to `false` so they
  * get the Object implementation, which is slower but behaves correctly.
  */
-Buffer.TYPED_ARRAY_SUPPORT = global.TYPED_ARRAY_SUPPORT !== undefined
-  ? global.TYPED_ARRAY_SUPPORT
+Buffer.TYPED_ARRAY_SUPPORT = globalThis.TYPED_ARRAY_SUPPORT !== undefined
+  ? globalThis.TYPED_ARRAY_SUPPORT
   : true
 
 /*
