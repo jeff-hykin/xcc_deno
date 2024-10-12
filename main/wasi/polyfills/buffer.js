9c8124a834881aeaad2c49cca30b8ecd72c3a55e
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-12 16:38:27 -0500

    fix the buffer import

--- main/wasi/polyfills/buffer.js
+++ main/wasi/polyfills/buffer.js
@@ -2,6 +2,8 @@
 "use strict";
 // Return our buffer depending on browser or node
 Object.defineProperty(exports, "__esModule", { value: true });
+// import { Buffer } from "https://cdn.jsdelivr.net/npm/buffer-es6@4.9.3/index.js"
+import { Buffer } from "../../buffer_es6@4_9_3.js"
 /*ROLLUP_REPLACE_BROWSER
 // @ts-ignore
 import { Buffer } from "buffer-es6";
