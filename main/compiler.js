790f1f38a89e9a30208f891464d82f64e1010180
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-14 10:35:46 -0500

    organize methods

--- main/compiler.js
+++ main/compiler.js
@@ -93,110 +93,123 @@ export class WccRunner {
 
         await this.chdir(this.pwd)
     }
-
-    /**
-     * Writes content to a file at the specified path.
-     *
-     * @param {string} filePath - The path of the file to write to.
-     * @param {string | Uint8Array} content - The content to write to the file.
-     * @returns {Promise<void>} A promise that resolves when the file has been written.
-     */
-    async writeFile(filePath, content) {
-        await this._postMessage("writeFile", { filePath: this.abspath(filePath), content });
-    }
-
-    /**
-     * Reads the content of a file at the specified path.
-     *
-     * @param {string} filePath - The path of the file to read from.
-     * @returns {Promise<Uint8Array>} A promise that resolves with the content of the file.
-     */
-    async readFile(filePath) {
-        return await this._postMessage("readFile", { filePath: this.abspath(filePath) });
-    }
-
-    /**
-     * Changes the current working directory to the specified path.
-     *
-     * @param {string} filePath - The path of the directory to change to.
-     * @returns {Promise<boolean>} A promise that resolves to true if the directory was successfully changed, false otherwise.
-     */
-    chdir(filePath) {
-        return this._postMessage("chdir", { filePath: this.abspath(filePath) });
-    }
-
-    /**
-     * Creates a new directory at the specified path.
-     *
-     * @param {string} filePath - The path of the directory to create.
-     * @param {any} [option] - Optional settings for the directory creation (e.g., recursive).
-     * @returns {Promise<void>} A promise that resolves when the directory has been created.
-     */
-    mkdir(filePath, option) {
-        return this._postMessage("mkdir", { filePath: this.abspath(filePath), option });
-    }
     
-    /**
-     * Compiles a source file using the specified options.
-     *
-     * @param {string} sourceName - The name of the source file to compile.
-     * @param {string[]} [extraOptions] - Optional additional options for the compiler.
-     * @returns {Promise<number>} A promise that resolves to the exit code of the compilation process.
-     */
-    function compile(sourceName, extraOptions) {
-        let args = [CC_PATH];
-        if (extraOptions != null) {
-            args = args.concat(extraOptions);
+    // 
+    // 
+    // main api
+    // 
+    // 
+        /**
+         * Compiles a source file using the specified options.
+         *
+         * @param {string} sourceName - The name of the source file to compile.
+         * @param {string[]} [extraOptions] - Optional additional options for the compiler.
+         * @returns {Promise<number>} A promise that resolves to the exit code of the compilation process.
+         */
+        function compile(sourceName, extraOptions) {
+            let args = [CC_PATH];
+            if (extraOptions != null) {
+                args = args.concat(extraOptions);
+            }
+            args.push(sourceName);
+
+            return this.runWasi(args[0], args);
         }
-        args.push(sourceName);
 
-        return this.runWasi(args[0], args);
-    }
-
-    /**
-     * Runs a WASI (WebAssembly System Interface) command with the specified arguments.
-     *
-     * @param {string} filePath - The path to the WASI executable.
-     * @param {string[]} args - The arguments to pass to the executable.
-     * @returns {Promise<number>} A promise that resolves to the exit code of the command.
-     */
-    async function runWasi(filePath, args) {
-        return await this._postMessage("runWasi", { filePath, args });
-    }
-
-    /**
-     * Clears temporary files from the temporary directory.
-     *
-     * @returns {Promise<void>} A promise that resolves when all temporary files have been deleted.
-     */
-    async function clearTemporaries() {
-        const files = await this._postMessage("readdir", { filePath: TMP_PATH });
-        await Promise.all(files.map((file) => this._postMessage("unlink", { filePath: `${TMP_PATH}/${file}` })));
-    }
-
-    /**
-     * Sends a message to the worker with the specified action and data.
-     *
-     * @param {string} action - The action to perform.
-     * @param {any} [data={}] - The data to send along with the action.
-     * @returns {Promise<any>} A promise that resolves with the response or rejects on error.
-     */
-    function _postMessage(action, data = {}) {
-        return new Promise((resolve, reject) => {
-            const messageId = ++this._messageId;
-            this._actionHandlerMap.set(messageId, { resolve, reject });
-
-            data.action = action;
-            data.messageId = messageId;
-            this._worker.postMessage(data);
-        });
-    }
-
-
-    abspath(path2) {
-        if (path2[0] === "/") {
-            return path2
+        /**
+         * Runs a WASI (WebAssembly System Interface) command with the specified arguments.
+         *
+         * @param {string} filePath - The path to the WASI executable.
+         * @param {string[]} args - The arguments to pass to the executable.
+         * @returns {Promise<number>} A promise that resolves to the exit code of the command.
+         */
+        async function runWasi(filePath, args) {
+            return await this._postMessage("runWasi", { filePath, args });
         }
-        return path.join(this.pwd, path2)
-    }
+
+        /**
+         * Clears temporary files from the temporary directory.
+         *
+         * @returns {Promise<void>} A promise that resolves when all temporary files have been deleted.
+         */
+        async function clearTemporaries() {
+            const files = await this._postMessage("readdir", { filePath: TMP_PATH });
+            await Promise.all(files.map((file) => this._postMessage("unlink", { filePath: `${TMP_PATH}/${file}` })));
+        }
+    
+    // 
+    // 
+    // file system tools
+    // 
+    // 
+        /**
+         * Writes content to a file at the specified path.
+         *
+         * @param {string} filePath - The path of the file to write to.
+         * @param {string | Uint8Array} content - The content to write to the file.
+         * @returns {Promise<void>} A promise that resolves when the file has been written.
+         */
+        async writeFile(filePath, content) {
+            await this._postMessage("writeFile", { filePath: this.abspath(filePath), content });
+        }
+
+        /**
+         * Reads the content of a file at the specified path.
+         *
+         * @param {string} filePath - The path of the file to read from.
+         * @returns {Promise<Uint8Array>} A promise that resolves with the content of the file.
+         */
+        async readFile(filePath) {
+            return await this._postMessage("readFile", { filePath: this.abspath(filePath) });
+        }
+
+        /**
+         * Changes the current working directory to the specified path.
+         *
+         * @param {string} filePath - The path of the directory to change to.
+         * @returns {Promise<boolean>} A promise that resolves to true if the directory was successfully changed, false otherwise.
+         */
+        chdir(filePath) {
+            return this._postMessage("chdir", { filePath: this.abspath(filePath) });
+        }
+
+        /**
+         * Creates a new directory at the specified path.
+         *
+         * @param {string} filePath - The path of the directory to create.
+         * @param {any} [option] - Optional settings for the directory creation (e.g., recursive).
+         * @returns {Promise<void>} A promise that resolves when the directory has been created.
+         */
+        mkdir(filePath, option) {
+            return this._postMessage("mkdir", { filePath: this.abspath(filePath), option });
+        }
+
+        abspath(path2) {
+            if (path2[0] === "/") {
+                return path2
+            }
+            return path.join(this.pwd, path2)
+        }
+    
+    // 
+    // interals 
+    // 
+    
+        /**
+         * Sends a message to the worker with the specified action and data.
+         *
+         * @param {string} action - The action to perform.
+         * @param {any} [data={}] - The data to send along with the action.
+         * @returns {Promise<any>} A promise that resolves with the response or rejects on error.
+         */
+        function _postMessage(action, data = {}) {
+            return new Promise((resolve, reject) => {
+                const messageId = ++this._messageId;
+                this._actionHandlerMap.set(messageId, { resolve, reject });
+
+                data.action = action;
+                data.messageId = messageId;
+                this._worker.postMessage(data);
+            });
+        }
 }
