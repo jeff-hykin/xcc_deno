fa270f2f10fae9dae6a6aba8207122001f0a64cd
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-14 13:03:58 -0500

    add export test

---
+++ tests/export_test.js
@@ -0,0 +1,85 @@
+import * as xcc from '../main/main.js'
+// import * as xcc from '../main/main.bundle.js'
+// import { WccRunner, DisWasm } from '../main/main.js'
+
+import { WASI } from "../main/wasi/index.js"
+import { WasmFs } from "../main/wasmfs/index.js"
+import path from "../main/node_shims/path.js"
+
+// 
+// setup imports
+// 
+    var wasmFs = new WasmFs()
+    var wasi = new WASI({
+        args: [],
+        bindings: {
+            ...WASI.defaultBindings,
+            fs: wasmFs.fs,
+            path,
+        },
+        preopens: {
+            "/": "/",
+            ".": "/",
+        },
+    })
+    var imports = {
+        wasi_snapshot_preview1: wasi.wasiImport,
+    }
+
+async function compileAndOrRun({sourceCode}) {
+    var wccRunner = await xcc.Compiler()
+    
+    const sourceName = "main.c"
+    const objFn = "main.o"
+    const compiledPath = "a.wasm"
+    const extraOptions = compileAndDump ? ["-efib", "-c", "-o", objFn, "--import-module-name=env", ] : undefined
+    await wccRunner.writeFile(sourceName, sourceCode)
+
+    const exitCode = await wccRunner.compile(sourceName, extraOptions)
+    if (exitCode !== 0) {
+        throw new Error(`Compile failed: ${exitCode}`)
+    }
+
+    const compiledCode = await wccRunner.readFile(objFn)
+    console.debug(`compiledCode.buffer is:`,compiledCode.buffer)
+    const disWasm = new xcc.DisWasm(compiledCode.buffer)
+    let compiledWasm = ""
+    disWasm.setLogFunc((code)=>compiledWasm+=(code+"\n"))
+    // disWasm.setLogFunc((s) => Util.putTerminal(`${s}\n`))
+    disWasm.dump()
+    console.debug(`compiledWasm is:`,compiledWasm)
+    const localImports = {
+        ...imports,
+        env: {
+            ...imports.env,
+                __linear_memory: new WebAssembly.Memory({ initial: 65536 /*65536 is max size*/ }),
+            __stack_pointer: new WebAssembly.Global({ value: "i32", mutable: true }, 0),
+        },
+    }
+    console.log(`localImports is:`,localImports)
+
+    var wasmModule = await WebAssembly.instantiate(compiledCode.buffer, localImports)
+    console.debug(`wasmModule is:`,wasmModule)
+    console.debug(`wasmModule.module is:`,wasmModule.module)
+    console.debug(`wasmModule.instance is:`,wasmModule.instance)
+    console.debug(`wasmModule.instance.exports is:`,wasmModule.instance.exports)
+    return wasmModule
+}
+
+const EXAMPLE_CODE = `
+int fib(int n) {
+    if (n < 2)
+        return n;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+`
+
+try {
+    console.log(await compileAndOrRun({sourceCode: EXAMPLE_CODE}))
+} catch (error) {
+    console.error(error)
+    console.error(error.stack)
+    Deno.exit(1)
+}
+Deno.exit()
