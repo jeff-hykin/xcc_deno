23b6eaaa5e567a03eae9ec213221884f94b37a2e
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-27 10:01:17 -0500

    update imports

--- main/js_box/null_env.js
+++ main/js_box/null_env.js
@@ -1,13 +1,14 @@
-import { capitalize, indent, toCamelCase, digitsToEnglishArray, toPascalCase, toKebabCase, toSnakeCase, toScreamingKebabCase, toScreamingSnakeCase, toRepresentation, toString, regex, findAll, iterativelyFindAll, escapeRegexMatch, escapeRegexReplace, extractFirst, isValidIdentifier, removeCommonPrefix, didYouMean } from "https://deno.land/x/good@1.11.0.0/string.js"
-import { deepCopy, deepCopySymbol, allKeyDescriptions, deepSortObject, shallowSortObject, isGeneratorObject,isAsyncIterable, isSyncIterable, isIterableTechnically, isSyncIterableObjectOrContainer, allKeys } from "https://deno.land/x/good@1.11.0.0/value.js"
-import { iter, next, Stop, Iterable, map, filter, reduce, frequencyCount, zip, count, enumerate, permute, combinations, slices, asyncIteratorToList, concurrentlyTransform, forkBy } from "https://deno.land/x/good@1.11.0.0/iterable.js"
+// import { capitalize, indent, toCamelCase, digitsToEnglishArray, toPascalCase, toKebabCase, toSnakeCase, toScreamingKebabCase, toScreamingSnakeCase, toRepresentation, toString, regex, findAll, iterativelyFindAll, escapeRegexMatch, escapeRegexReplace, extractFirst, isValidIdentifier, removeCommonPrefix, didYouMean } from "https://deno.land/x/good@1.12.0.0/string.js"
+// import { deepCopy, deepCopySymbol, allKeyDescriptions, deepSortObject, shallowSortObject, isGeneratorObject,isAsyncIterable, isSyncIterable, isIterableTechnically, isSyncIterableObjectOrContainer, allKeys } from "https://deno.land/x/good@1.12.0.0/value.js"
+// import { iter, next, Stop, Iterable, map, filter, reduce, frequencyCount, zip, count, enumerate, permute, combinations, slices, asyncIteratorToList, concurrentlyTransform, forkBy } from "https://deno.land/x/good@1.12.0.0/iterable.js"
 import { RandomSource } from "./deterministic_tooling/random_seed.js"
 import { createTimingTools } from "./deterministic_tooling/timing_tools.js"
-import { TypedArray } from "https://deno.land/x/good@1.11.0.0/flattened/typed_array__class.js"
-import { recursivelyOwnKeysOf } from "https://deno.land/x/good@1.11.0.0/flattened/recursively_own_keys_of.js"
-import { set } from "https://deno.land/x/good@1.11.0.0/flattened/set.js"
-import { get } from "https://deno.land/x/good@1.11.0.0/flattened/get.js"
-import { subtract } from "https://deno.land/x/good@1.11.0.0/flattened/subtract.js"
+import { TypedArray } from "https://deno.land/x/good@1.12.0.0/flattened/typed_array__class.js"
+import { recursivelyOwnKeysOf } from "https://deno.land/x/good@1.12.0.0/flattened/recursively_own_keys_of.js"
+import { set } from "https://deno.land/x/good@1.12.0.0/flattened/set.js"
+import { get } from "https://deno.land/x/good@1.12.0.0/flattened/get.js"
+import { subtract } from "https://deno.land/x/good@1.12.0.0/flattened/subtract.js"
+import { toRepresentation } from "https://deno.land/x/good@1.12.0.0/flattened/to_representation.js"
 
 // Overview:
     // 1. Start with Object, Function, Array, Number, String, Boolean, Symbol, Error, Promise
@@ -20,7 +21,7 @@ import { subtract } from "https://deno.land/x/good@1.11.0.0/flattened/subtract.j
             // if so delete it
 
 
-import {nameMap} from "./whitelist_small.js"
+import { nameMap } from "./whitelist_small.js"
 import whitelist from "./whitelist_small.js"
 import { fail } from "../node_shims/assert.js"
 import { ensureDescriptorAgreement } from "./deterministic_tooling/ensure_descriptor_agreement.js"
