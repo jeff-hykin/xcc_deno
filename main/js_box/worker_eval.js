e453ef1919605ca169a8aca4e797feac67d8695d
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-27 16:40:37 -0500

    clean up

--- main/js_box/worker_eval.js
+++ main/js_box/worker_eval.js
@@ -6,26 +6,28 @@
  * await workerEval({world:{}, timeout:Infinity, untrustedCode:`10`})
  * ```
  */
-export function workerEval({timeout, untrustedCode}={timeout:Infinity}) {
+export function workerEval({ timeout, untrustedCode } = { timeout: Infinity }) {
     return new Promise(function (resolve, reject) {
         var blobURL = URL.createObjectURL(
             new Blob(
-                ["(", 
-                function() {
-                    var _postMessage = postMessage
-                    var _addEventListener = addEventListener
+                [
+                    "(",
+                    function () {
+                        // TODO: fix this data transfer method (there's got to be a better way)
+                        var _postMessage = postMessage
+                        var _addEventListener = addEventListener
 
-                    _addEventListener("message", function (e) {
-                        var f = new Function("", "return (" + e.data + "\n);")
-                        _postMessage(f())
-                    })
-                }.toString()
-                ,`)() // ${Math.random()}`, ],
+                        _addEventListener("message", function (e) {
+                            var f = new Function("", "return(" + e.data + "\n)")
+                            _postMessage(f())
+                        })
+                    }.toString(),
+                    `)()`,
+                ],
                 { type: "application/javascript" }
             )
         )
-        // TODO: clean up Math.random() (debugging stuff)
-        var worker = new Worker(blobURL+`#${Math.random()}`, { type: "module" })
+        var worker = new Worker(blobURL, { type: "module" })
         // URL.revokeObjectURL(blobURL) // For some reason this causes an error on Deno when calling function more than once
         worker.onmessage = function (evt) {
             worker.terminate()
@@ -35,7 +37,7 @@ export function workerEval({timeout, untrustedCode}={timeout:Infinity}) {
             reject(new Error(evt.message))
         }
         worker.postMessage(untrustedCode)
-        
+
         if (timeout !== Infinity) {
             setTimeout(function () {
                 worker.terminate()
