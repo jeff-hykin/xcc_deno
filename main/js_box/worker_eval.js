765391dbc399ec370f8d8e669bd21971d1673adb
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-28 12:44:40 -0500

    add live bundle for better debugging

--- main/js_box/worker_eval.js
+++ main/js_box/worker_eval.js
@@ -1,16 +1,56 @@
-import nullEnvCode from "./null_env.bundle.js.binaryified.js"
+import nullEnvCodeImport from "./null_env.bundle.js.binaryified.js"
 
-// function makeImport(codeString) {
-//     function replaceNonAsciiWithUnicode(str) {
-//         return str.replace(/[^\0-~](?<!\n|\r|\t|\0)/g, (char) => {
-//             return '\\u' + ('0000' + char.charCodeAt(0).toString(16)).slice(-4);
-//         })
-//     }
-// 
-//     return `import "data:text/javascript;base64,${btoa(replaceNonAsciiWithUnicode(codeString))}"`
-// }
-// // const nullEnvCode = new TextDecoder().decode(nullEnvBytes)
-// const importString = makeImport(nullEnvCode+'\nenforceNullEnv()')
+let nullEnvCode = nullEnvCodeImport
+// dynamic bundle and read-as-string for the worker
+if (typeof Deno !== "undefined") {
+    const { build, stop } = await import("https://deno.land/x/esbuild@v0.24.0/mod.js")
+    const { FileSystem } = await import("https://deno.land/x/quickr@0.6.72/main/file_system.js")
+    const { denoPlugins } = await import("https://esm.sh/jsr/@duesabati/esbuild-deno-plugin@0.1.0/mod.ts")
+    const targetToBuild = `${FileSystem.thisFolder}/support/enforce_null_env.js`
+    if ((await FileSystem.info(targetToBuild)).isFile) {
+        nullEnvCode = "{" + new TextDecoder().decode(await new Promise((resolve, reject)=>{
+            build({
+                bundle: true,
+                entryPoints: [targetToBuild],
+                jsxFactory: "h",
+                format: "esm",
+                write: false,
+                plugins: [
+                    {
+                        "name": "exit-on-build",
+                        "setup": (build) => {
+                            build.onEnd((result) => {
+                                if (result.errors.length > 0) {
+                                    reject(result.errors)
+                                    stop().catch(reject)
+                                    return
+                                }
+                                resolve(result.outputFiles[0].contents)
+                                stop().catch(reject)
+                            })
+                        },
+                    },
+                    ...denoPlugins(),
+                ],
+                external: [
+                    "node:assert",
+                ]
+            }).catch(reject)
+        }))+"}"
+        console.log(nullEnvCode.slice(0,100)+"..."+nullEnvCode.slice(-100))
+    }
+    // function makeImport(codeString) {
+    // function replaceNonAsciiWithUnicode(str) {
+    //     return str.replace(/[^\0-~](?<!\n|\r|\t|\0)/g, (char) => {
+    //         return '\\u' + ('0000' + char.charCodeAt(0).toString(16)).slice(-4);
+    //     })
+    // }
+
+    // return `import "data:text/javascript;base64,${btoa(replaceNonAsciiWithUnicode(codeString))}"`
+    // }
+    // // const nullEnvCode = new TextDecoder().decode(nullEnvBytes)
+    // const importString = makeImport(nullEnvCode+'\nenforceNullEnv()')
+}
 
 /**
  * run code in a web worker
